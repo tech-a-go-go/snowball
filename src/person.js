d46c62d5437ff8f837caccf99871a7bc96efc5c7
Fumitada Hattori <hattori@banana-systems.com>
2022-12-18 02:53:29 +0900

    person

---
+++ src/person.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    stringSentence() {
+      return "Hello, my name is " + this.name + " and I'm " + this.age;
+    }
+}
+  
