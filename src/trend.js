de6da25f129afbea82ad6e4242700f777c4fcf01
Fumitada Hattori <hattori@banana-systems.com>
2023-01-04 20:33:02 +0900

    getTrendPattern

--- src/trend.js
+++ src/trend.js
@@ -5,6 +5,7 @@ const Trend = {
     DOWN : 2,
   };
 
+
 function trend2str(trend) {
     if (trend == Trend.NONE) {
         return "NONE"
@@ -35,4 +36,32 @@ function checkTrend(shortMaList, longMaList) {
         return Trend.DOWN
     }
     return Trend.NONE
+}
+
+
+function getTrendPattern(ma1, ma2, ma3, ma4) {
+    if (ma1 > ma2 && ma2 > ma3 && ma3 > ma4) { return 1 }
+    else if (ma2 > ma1 && ma1 > ma3 && ma3 > ma4) { return 2 }
+    else if (ma2 > ma3 && ma3 > ma1 && ma1 > ma4) { return 3 }
+    else if (ma2 > ma3 && ma3 > ma4 && ma4 > ma1) { return 4 }
+    else if (ma3 > ma2 && ma2 > ma1 && ma1 > ma4) { return 5 }
+    else if (ma3 > ma2 && ma2 > ma4 && ma4 > ma1) { return 6 }
+    else if (ma3 > ma4 && ma4 > ma2 && ma2 > ma1) { return 7 }
+    else if (ma3 > ma4 && ma4 > ma1 && ma1 > ma2) { return 8 }
+    else if (ma3 > ma1 && ma1 > ma4 && ma4 > ma2) { return 9 }
+    else if (ma1 > ma3 && ma3 > ma4 && ma4 > ma2) { return 10 }
+    else if (ma3 > ma1 && ma1 > ma2 && ma2 > ma4) { return 11 }
+    else if (ma1 > ma3 && ma3 > ma2 && ma2 > ma4) { return 12 }
+    else if (ma4 > ma3 && ma3 > ma2 && ma2 > ma1) { return 21 }
+    else if (ma4 > ma3 && ma3 > ma1 && ma1 > ma2) { return 22 }
+    else if (ma4 > ma1 && ma1 > ma3 && ma3 > ma2) { return 23 }
+    else if (ma1 > ma4 && ma4 > ma3 && ma3 > ma2) { return 24 }
+    else if (ma4 > ma1 && ma1 > ma2 && ma2 > ma3) { return 25 }
+    else if (ma1 > ma4 && ma4 > ma2 && ma2 > ma3) { return 26 }
+    else if (ma1 > ma2 && ma2 > ma4 && ma4 > ma3) { return 27 }
+    else if (ma2 > ma1 && ma1 > ma4 && ma4 > ma3) { return 28 }
+    else if (ma2 > ma4 && ma4 > ma1 && ma1 > ma3) { return 29 }
+    else if (ma2 > ma4 && ma4 > ma3 && ma3 > ma1) { return 30 }
+    else if (ma4 > ma2 && ma2 > ma1 && ma1 > ma3) { return 31 }
+    else if (ma4 > ma2 && ma2 > ma3 && ma3 > ma1) { return 32 }
 }
