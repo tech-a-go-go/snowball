bfe347068c6300ff00f9a8d32b9f5beb192ad23c
Fumitada Hattori <hattori@banana-systems.com>
2023-10-08 23:48:43 +0900

    refactor

--- src/trend_manager.js
+++ src/trend_manager.js
@@ -93,40 +93,28 @@ class TrendManager {
     addPrices2(time, order) {
         if (order === ASCENDING_ORDER) {
             if (this.currentState !== TREND_DOWN_STATE) {
-                // 下降トレンド開始
                 if(this.currentState === TREND_UP_STATE){
-                    // 上昇トレンドが終了し下降トレンド開始なので上昇トレンドのTimeRangeを追加
-                    this.trendUpTimeRanges.push(new TimeRange([this.beginTime, time]))
+                    this.trendUpTimeRanges.push(new TimeRange(this.beginTime, time))
                 }
                 this.currentState = TREND_DOWN_STATE
                 this.beginTime = time
             }
         } else if (order === DESCENDING_ORDER) {
             if (this.currentState !== TREND_UP_STATE) {
-                // 上昇トレンド開始
                 if(this.currentState === TREND_DOWN_STATE){
-                    // 下降トレンドが終了し上昇トレンドが開始なので下降トレンドのTimeRangeを追加
-                    this.trendDownTimeRanges.push(new TimeRange([this.beginTime, time]))
+                    this.trendDownTimeRanges.push(new TimeRange(this.beginTime, time))
                 }
                 this.currentState = TREND_UP_STATE
                 this.beginTime = time
             }
         } else {
             if (this.currentState === TREND_UP_STATE) {
-                // 上昇トレンド終了
                 this.currentState = NO_TREND_STATE
-                this.trendUpTimeRanges.push(new TimeRange([this.beginTime, time]))
-                // console.log("finish TREND_UP_STATE")
-                // console.log("prices", prices)
-
+                this.trendUpTimeRanges.push(new TimeRange(this.beginTime, time))
                 this.beginTime = null
             } else if (this.currentState === TREND_DOWN_STATE) {
-                // 下降トレンド終了
                 this.currentState = NO_TREND_STATE
-                this.trendDownTimeRanges.push(new TimeRange([this.beginTime, time]))
-                // console.log("finish TREND_DOWN_STATE")
-                // console.log("prices", prices)
-
+                this.trendDownTimeRanges.push(new TimeRange(this.beginTime, time))
                 this.beginTime = null
             }
         }
